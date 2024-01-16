65d663c74a748d3209ce552d6d9642be1c6f767f
Yair Levi <yairlevi2001@gmail.com>
2024-01-16 10:36:08 +0200

    comment out- jitpack includes tests?

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -2,17 +2,17 @@ import org.levi.coffee.Window
 
 
 fun main() {
-    val win = Window(isDev = true)
-    win.setSize(700, 700)
-    win.setTitle("My first Javatron app!")
-
-    win.setURL("http://localhost:5173")
-    win.bind(
-        Calculator(),
-    )
-
-    win.addBeforeStartCallback { println("Started app...") }
-    win.addOnCloseCallback { println("Closed the app!") }
-
-    win.run()
+//    val win = Window(isDev = true)
+//    win.setSize(700, 700)
+//    win.setTitle("My first Javatron app!")
+//
+//    win.setURL("http://localhost:5173")
+//    win.bind(
+//        Calculator(),
+//    )
+//
+//    win.addBeforeStartCallback { println("Started app...") }
+//    win.addOnCloseCallback { println("Closed the app!") }
+//
+//    win.run()
 }
