84e13ef925518e64f12823c210422d87231f1e5b
Yair Levi <yairlevi2001@gmail.com>
2024-01-28 21:34:05 +0200

    update templates for testing.

--- cli/coffee/templates/backend/java/src/main/java/Main.java
+++ cli/coffee/templates/backend/java/src/main/java/Main.java
@@ -3,9 +3,10 @@ import org.levi.coffee.Window;
 
 public class Main {
     public static void main(String[] args) {
-        Window w = new Window(true);
+        Window w = new Window(args);
         w.setSize(800, 600);
         w.setTitle("Java coffee app!");
+        w.setURL("http://localhost:5173");
         w.bind(new App());
         w.run();
     }
