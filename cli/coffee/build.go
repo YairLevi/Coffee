8fbb92bc1f3b4c989a239afdf8e1ab9c72b1b442
Yair Levi <yairlevi2001@gmail.com>
2024-03-15 15:16:54 +0200

    fix generation and install deps before bundle

--- cli/coffee/build.go
+++ cli/coffee/build.go
@@ -12,9 +12,41 @@ var sourceDirMapping = map[string]string{
 }
 
 func Build() {
-	err := os.Chdir("frontend")
+	_, err := RunCommand(CmdProps{
+		Cmd:       CompileBackend,
+		Sync:      true,
+		Opts:      Opts(WithStderr, WithStdout),
+		LogBefore: "Compiling Application",
+	})
+	if err != nil {
+		log.Error("Failed to compile backend code.", "err", err)
+		return
+	}
+
+	_, err = RunCommand(CmdProps{
+		Cmd:       GenerateAppBinds,
+		LogBefore: "Generating type-safe frontend bindings...",
+		Sync:      true,
+		Opts:      Opts(WithStderr),
+	})
+	if err != nil {
+		log.Errorf("Failed to create bindings. %v", err)
+		return
+	}
+
+	err = os.Chdir("frontend")
 	if err != nil {
 		log.Errorf("error: %v", err)
+		return
+	}
+
+	_, err = RunCommand(CmdProps{
+		Cmd:       InstallFrontendDependencies,
+		LogBefore: "Installing dependencies",
+		Sync:      true,
+	})
+	if err != nil {
+		log.Error("Failed to download NPM dependencies.", "err", err)
 		return
 	}
 
